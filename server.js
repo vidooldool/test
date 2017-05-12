f617866d38a2338f9c5bbedc4e491596058d5c29
vidooldool <bidooldool@naver.com>
2017-05-13 03:12:24 +0900

    test vidool

---
+++ server.js
@@ -0,0 +1,10 @@
+  var express = require('express');
+  var app = express();
+
+  app.get('/', function(req, res){
+      res.send('Hello NAVER Cloud Platform!!');
+  });
+
+  app.listen(8000, function(){
+      console.log('Connected!!');
+  });
