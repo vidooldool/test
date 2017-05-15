5e7574d9b95c11421978a12ec7707d3ac0c4dd64
vidooldool <bidooldool@naver.com>
2017-05-15 23:21:42 +0900

    mina

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
   var express = require('express');
   var app = express();
+  var fs = require('fs');
 
   app.get('/', function(req, res){
       //res.send('Hello NAVER Cloud Platform!!');
       res.send('by bidooldool ~');
+      res._write
       //res.send('changed');
   });
 
   app.get('/cello', function(req, res){
       //res.send('Hello NAVER Cloud Platform!!');
-      res.send('hi cello');
+      res.send('hi cello <br> I love 민아찡');
+      fs.readFile('minazzing.jpg', function(error, data){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.end(data);
+      });
       //res.send('changed');
   });
 
